f935b28a498932b732bb301d16b588397c48ffae
raphaelketere <103488364+raphaelketere@users.noreply.github.com>
2023-06-19 10:49:31 +0300

    added new branch
    successfully added the branch

--- zuri/manage.py
+++ zuri/manage.py
@@ -4,8 +4,10 @@ import os
 import sys
 
 
+
 def main():
     """Run administrative tasks."""
+    # sys.path.append('..')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zuri.settings')
     try:
         from django.core.management import execute_from_command_line
--- zuri/myapp/models.py
+++ zuri/myapp/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.db import models 
 
 # Create your models here.
+
+import datetime
+import os
+
+
+def filepath (request, filename):
+    old_filename = filename
+    timeNow = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    filename = '%s%s', (timeNow, old_filename)
+    return os.path.join('uploads/',filename)
+
+class Vehicles(models.Model):
+    name = models.TextField(max_length=190)
+    price = models.TextField(max_length=100)
+    descriptions = models.TextField(max_length=500, null=True)
+    image = models.ImageField(upload_to=filepath, null=True, blank=True)
